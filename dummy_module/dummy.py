0b0ca81e978aa415e8db47d7d74afc79277855da
David Shi <david.shi@adeptmind.ai>
2018-03-06 15:13:56 -0500

    Add doc string for fib_decider

--- dummy_module/dummy.py
+++ dummy_module/dummy.py
@@ -26,6 +26,12 @@ def fib_iter(n: int) -> int:
 
 
 def fib_decider(n: int, to_call: str = 'fib') -> int:
+    """
+    Gives option to call which fibonacci function to call
+    :param n: position in the sequence to get
+    :param to_call: which function to call
+    :return: nth fibonacci number
+    """
     if to_call == 'fib':
         return fib(n)
     return fib_iter(n)
