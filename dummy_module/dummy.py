fc3017d709b1a98a85b620b1fdaba45bf5230a45
David Shi <david.shi@adeptmind.ai>
2018-03-06 15:06:58 -0500

    Add dummy files

---
+++ dummy_module/dummy.py
@@ -0,0 +1,25 @@
+def fib(n: int) -> int:
+    """
+    Get the nth fibonacci number
+    :param n: index in the sequence to get
+    :return: nth fibonacci number in the sequence
+    """
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def fib_iter(n: int) -> int:
+    """
+    Get the nth fibonacci number iteratively
+    :param n: index in the sequence to get
+    :return: nth fibonacci number in the sequence
+    """
+    a, b, c = 0, 1, 1
+    for i in range(0, n):
+        a = b
+        b = c
+        c = a + b
+
+    return c
